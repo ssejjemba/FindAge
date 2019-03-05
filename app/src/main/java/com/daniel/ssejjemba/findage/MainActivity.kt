32751ef4a74b98ade445ff2bd3b16e01eb67b5ea
Daniel <ssejjembadan@gmail.com>
2019-03-05 15:38:31 +0300

    Welcome page completed
    Signed-off-by: Daniel <ssejjembadan@gmail.com>

--- app/src/main/java/com/daniel/ssejjemba/findage/MainActivity.kt
+++ app/src/main/java/com/daniel/ssejjemba/findage/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.daniel.ssejjemba.findage
 
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.res.ResourcesCompat
+import android.view.View
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_continue -> {
+
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val typeFace: Typeface? = ResourcesCompat.getFont(this, R.font.Ginga)
+        val appName:TextView = findViewById(R.id.tv_app_name)
+        appName.typeface = typeFace
     }
 }
