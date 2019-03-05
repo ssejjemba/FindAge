efcbe2871f01be1b53ad4a5c43e74cf219c84094
Daniel <ssejjembadan@gmail.com>
2019-03-05 17:18:47 +0300

    Game Page Layout finished

--- app/src/main/java/com/daniel/ssejjemba/findage/MainActivity.kt
+++ app/src/main/java/com/daniel/ssejjemba/findage/MainActivity.kt
@@ -5,26 +5,21 @@ import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.res.ResourcesCompat
-import android.view.View
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.btn_continue -> {
-                val intent = Intent(this, GameActivity::class.java)
-                startActivity(intent)
-            }
-        }
-    }
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val typeFace: Typeface? = ResourcesCompat.getFont(this, R.font.Ginga)
+        val typeFace: Typeface? = ResourcesCompat.getFont(this, R.font.ginga)
         tv_app_name.typeface = typeFace
+
+        btn_continue.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
